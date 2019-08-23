fc23351d87dc5b6136638da8bc3a594fbe373ec1
Britone Mwasaru <bmwasaru@gmail.com>
2019-08-23 16:44:40 +0300

    add save tweet text and id to mysql

--- mombasani.py
+++ mombasani.py
@@ -1,16 +1,26 @@
+from __future__ import unicode_literals
 import os
-
 import json
 import logging
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, API
 from tweepy import Stream
+import mysql.connector
 
 access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 consumer_key = os.environ.get('consumer_key')
 consumer_secret = os.environ.get('consumer_secret')
+
+db = mysql.connector.connect(
+    host="localhost",
+    user=os.environ.get('user'),
+    passwd=os.environ.get('passwd'),
+    database="mombasani"
+)
+
+db_cursor = db.cursor()
 
 auth_handler = OAuthHandler(consumer_key, consumer_secret)
 auth_handler.set_access_token(access_token, access_token_secret)
@@ -21,8 +31,11 @@ api = API(auth_handler)
 class TweetsListener(StreamListener):
     def on_data(self, data):
         tweet = json.loads(data)
+        sql = "INSERT INTO tweets (tweet_id, tweet_text) VALUES (%s, %s)"
+        values = (tweet['id'], tweet['text'])
+        db_cursor.execute(sql, values)
+        db.commit()
         try:
-            api.retweet(tweet['id'])
             logging.info("RT: {}".format(tweet['text']))
         except Exception as e:
             logging.error(e)
